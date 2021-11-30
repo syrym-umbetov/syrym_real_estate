9c5cdfb2f040e40caa23ff6a97b34aca70a84235
syrym-umbetov <umbetov.syrym@gmail.com>
2021-11-30 20:10:38 +0600

    property

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
-
+import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
@@ -33,7 +33,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
       <Flex flexWrap="wrap">
-        {/* Fetch the properties and map over them... */}
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
       </Flex>
 
       <Banner 
@@ -46,7 +46,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
      />
-        {/* Fetch the properties and map over them... */}
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
     </Box>
   )
 }
